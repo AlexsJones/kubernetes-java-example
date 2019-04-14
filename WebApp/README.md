f3a5c6b7702f709518d2a58d671e6c50f18fb1b9
AlexsJones <alexsimonjones@gmail.com>
2019-04-14 21:42:26 +0100

    added java project

---
+++ WebApp/README.md
@@ -0,0 +1,17 @@
+# Webapp
+
+Spring-boot
+
+
+## Build an image
+
+`docker build -t myimage.`
+
+and to run
+
+`docker run -p 8083:8083 <imageid>`
+
+
+## Usage
+
+`http://localhost:8083/`
---
+++ WebApp/src/main/java/com/WebApp/EntryPoint/Entry.java
@@ -0,0 +1,37 @@
+package com.WebApp.EntryPoint;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(scanBasePackages = {"com.WebApp"})
+public class Entry {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Entry.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
+}
